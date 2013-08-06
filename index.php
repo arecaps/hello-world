3d2b5701764491d73a648b45800f25a56151fdb1
arecaps <arecaplan@gmail.com>
2013-08-06 19:11:45 -0400

    added

--- index.php
+++ index.php
@@ -9,21 +9,25 @@
      array(
       'name' => 'Londinski',
       'address' => "11 Kew Gardens",
-      'phone' => 7301373,
+      'phone' => 7791499,
     ),
      array(
       'name' => 'Bernfeld',
       'address' => "13 Kew Gardens",
-      'phone' => 7301373,
+      'phone' => 1234567,
     ),
    array(
       'name' => 'BenEza',
       'address' => "7 Kew Gardens",
-      'phone' => 7301373,
+      'phone' => 3644722,
     ),
   );
 
   $addresses = array_column($records, 'address');
-print_r($addresses);
+  print_r($addresses);
+
+  $nameNumber = array_column($records, 'name', 'phone')
+  print_r($nameNumber);
+
 
 ?>
