d4f9f6ffb33480199d808a0cc1c58c283d0f4b01
arecaps <arecaplan@gmail.com>
2013-08-09 01:52:04 -0400

    still trying

--- practice.php
+++ practice.php
@@ -1,7 +1,9 @@
 <?php
-  class myClass() {
-
-  'What is this going to do?';
+  class myClass {
+    function __construct()
+      $surprise = 'What is this going to do';
+    
+  echo "$surprise";
   }
 
   $obj = new myClass;
