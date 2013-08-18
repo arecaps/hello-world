b1d4b673edeaaa632ab4b58f41ac2294fe088a2e
arecaps <arecaplan@gmail.com>
2013-08-18 19:23:46 -0400

    more cool stuff

--- variables.php
+++ variables.php
@@ -8,6 +8,14 @@
     echo strpos($fullName, "l"). '<br>';
 
       $test = strpos($fullName, "p");
-        echo $test;
+      $test2 = strpos($fullName, "n");
 
+        echo $test. '<br>';
+        echo $test2. '<br>';
+
+          echo $test++. '<br>';
+          echo $test++. '<br>';
+
+             echo $test+$test. '<br>';
+             echo $test2+$test2. '<br>';
 ?>
