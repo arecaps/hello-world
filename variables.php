98318c3661a18723340887fb57f2e22126926fe5
arecaps <arecaplan@gmail.com>
2013-08-18 02:20:56 -0400

    cool stuff

---
+++ variables.php
@@ -0,0 +1,13 @@
+<?php
+  $firstName = "aryeh";
+  $lastName = "caplan";
+  $fullName = $firstName. " " . $lastName;
+
+    echo $fullName. '<br>';
+    echo strchr($fullName, "a"). '<br>';
+    echo strpos($fullName, "l"). '<br>';
+
+      $test = strpos($fullName, "p");
+        echo $test;
+
+?>
